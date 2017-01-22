b1ad8f0901a96b25299a5abc63fa810d9c3b8909
Tyler Jones <tj111710@gmail.com>
2017-01-22 19:12:08 +0000

    Added custom URL for contact us

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
   
+  #__PAGES CONTROLELR
+  root to: 'pages#home'
   get 'about', to: 'pages#about'
+  
+  #___CONTACTS CONTROLLER
   resources :contacts
+  get 'contact-us', to: 'contacts#new'
 end
