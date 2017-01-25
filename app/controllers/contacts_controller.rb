52fbf46fa43ed50a313e47588f919e3142201eff
Tyler Jones <tj111710@gmail.com>
2017-01-25 01:54:45 +0000

    Trigger the mailer from the controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,18 @@ class ContactsController < ApplicationController
     def create
       @contact = Contact.new(contact_params)
         if @contact.save
+          #MAILER
+          name = params[:contact][:name]
+          email = params[:contact][:email]
+          body = params[:contacts][:comments]
+          ContactMailer.contact_email(name, email, body).deliver
+          
+          #REDIRECT SUCCESS
           flash[:success] = "Message sent"
           redirect_to new_contact_path
+          
         else
+          #REDIRECT WITH ERRORS
           flash[:danger] = @contact.errors.full_messages.join(", ")
           redirect_to new_contact_path
         end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default to: 'tyler.apmotors@gmail.com'
   
-  def contact-email(name, email, body)
+  def contact_email(name, email, body)
     @name = name
     @email - email
     @body - body
